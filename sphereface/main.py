6ff5408b4fb7943d126e3f01cc5e932725a1c738
blakemr <matthewblake0625@gmail.com>
2021-05-28 10:30:27 -0400

    added great circle distance function.

--- sphereface/main.py
+++ sphereface/main.py
@@ -1,8 +1,11 @@
 from typing import Tuple
+from math import radians, sin, cos
+
+EARTH_RADIUS_METERS = 6371000
 
 
 def dms2dd(d: float, m: float, s: float, sign: bool) -> float:
-    '''Converts degree, minutes, seconds coordinates to decimal degree format
+    ''' Converts degree, minutes, seconds coordinates to decimal degree format
 
     args:
         d, m, s (float): degrees, minutes, seconds
@@ -17,3 +20,32 @@ def dms2dd(d: float, m: float, s: float, sign: bool) -> float:
     dh = 1 if sign else -1
 
     return (dd + dm + ds) * dh
+
+
+def great_circle_distance(p1: Tuple, p2: Tuple) -> float:
+    ''' Returns the shortest distance between two points on a sphere
+
+    args:
+        p1, p2 (lat, lon): latitude, longitude tuples for
+            each point in decimal degrees
+
+    return:
+        distance between the two points in meters
+    '''
+    lat1, lon1 = p1
+    lat2, lon2 = p2
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    # Haversine
+    dla = lat1 - lat2
+    dlo = lon1 - lon2
+
+    h = sin(dla / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlo / 2) ** 2
+
+    r2 = 2 * EARTH_RADIUS_METERS
+
+    return h * r2
